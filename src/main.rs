68118e8b1299fb2f971f339b78707c06860a3462
Alexander Serowy <serowy@hotmail.com>
2021-02-09 17:23:05 +0100

    added devcontainer

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+extern crate rs_docker;
+
+use rs_docker::Docker;
+
 fn main() {
-    println!("Hello, world!");
+    let mut docker = match Docker::connect("unix:///var/run/docker.sock") {
+        Ok(docker) => docker,
+        Err(e) => {
+            panic!("{}", e);
+        }
+    };
+
+    let containers = match docker.get_containers(false) {
+        Ok(containers) => containers,
+        Err(e) => {
+            panic!("{}", e);
+        }
+    };
 }
