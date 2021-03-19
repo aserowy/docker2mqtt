e29110245c52bc94aeec541c51db2df69c1c2c9a
Alexander Serowy <serowy@hotmail.com>
2021-03-19 06:12:22 +0100

    refactored mod into more testable funcs

--- src/docker/events.rs
+++ src/docker/events.rs
@@ -7,35 +7,49 @@ use bollard::{
     Docker,
 };
 use tokio::{sync::broadcast, task};
-use tokio_stream::StreamExt;
+use tokio_stream::{Stream, StreamExt};
 use tracing::error;
 
 use super::{ContainerEvent, Event, EventType};
 
 pub async fn source(event_sender: broadcast::Sender<Event>, client: Docker) {
     task::spawn(async move {
-        let mut query = HashMap::new();
-        query.insert("type".to_owned(), vec!["container".to_owned()]);
+        let stream = get_event_response_stream(client).filter_map(response_to_events);
 
-        let filter = Some(EventsOptions::<String> {
-            since: None,
-            until: None,
-            filters: query,
-        });
-
-        let mut stream = client.events(filter).filter_map(get_events);
-        while let Some(events) = stream.next().await {
-            for event in events.into_iter() {
-                match event_sender.send(event) {
-                    Ok(_) => {}
-                    Err(e) => error!("event could not be send to event_router: {}", e),
-                }
-            }
-        }
+        receive_loop(stream, event_sender).await
     });
 }
 
-fn get_events(result: Result<SystemEventsResponse, Error>) -> Option<Vec<Event>> {
+async fn receive_loop(
+    mut stream: impl Stream<Item = Vec<Event>> + Unpin,
+    event_sender: broadcast::Sender<Event>,
+) {
+    while let Some(events) = stream.next().await {
+        for event in events.into_iter() {
+            match event_sender.send(event) {
+                Ok(_) => {}
+                Err(e) => error!("event could not be send to event_router: {}", e),
+            }
+        }
+    }
+}
+
+fn get_event_response_stream(
+    client: Docker,
+) -> impl Stream<Item = Result<SystemEventsResponse, Error>> {
+    let mut query = HashMap::new();
+    query.insert("type".to_owned(), vec!["container".to_owned()]);
+
+    let filter = Some(EventsOptions::<String> {
+        since: None,
+        until: None,
+        filters: query,
+    });
+
+    client.events(filter)
+}
+
+fn response_to_events(result: Result<SystemEventsResponse, Error>) -> Option<Vec<Event>> {
     let response: SystemEventsResponse;
     match result {
         Ok(rspns) => response = rspns,
@@ -60,11 +74,9 @@ fn get_events(result: Result<SystemEventsResponse, Error>) -> Option<Vec<Event>>
 }
 
 fn get_state_event(response: &SystemEventsResponse) -> Event {
-    let container_event = get_container_event(&response.action);
-
     Event {
         container_name: get_attribute(&response.actor, "name"),
-        event: EventType::State(container_event),
+        event: EventType::State(get_container_event(&response.action)),
     }
 }
 
@@ -90,30 +102,16 @@ fn get_attribute(actor: &Option<SystemEventsResponseActor>, attribute: &str) -> 
 
 fn get_container_event(action: &Option<String>) -> ContainerEvent {
     match action.as_deref() {
-        // Some("attach") => ContainerEvent::Attach,
-        // Some("commit") => ContainerEvent::Commit,
-        // Some("copy") => ContainerEvent::Copy,
         Some("create") => ContainerEvent::Create,
         Some("destroy") => ContainerEvent::Destroy,
-        // Some("detach") => ContainerEvent::Detach,
         Some("die") => ContainerEvent::Die,
-        // Some("exec_create") => ContainerEvent::Exec_create,
-        // Some("exec_detach") => ContainerEvent::Exec_detach,
-        // Some("exec_start") => ContainerEvent::Exec_start,
-        // Some("exec_die") => ContainerEvent::Exec_die,
-        // Some("export") => ContainerEvent::Export,
-        // Some("health_status") => ContainerEvent::Health_status,
         Some("kill") => ContainerEvent::Kill,
-        // Some("oom") => ContainerEvent::Oom,
         Some("pause") => ContainerEvent::Pause,
         Some("rename") => ContainerEvent::Rename,
-        // Some("resize") => ContainerEvent::Resize,
         Some("restart") => ContainerEvent::Restart,
         Some("start") => ContainerEvent::Start,
         Some("stop") => ContainerEvent::Stop,
-        // Some("top") => ContainerEvent::Top,
         Some("unpause") => ContainerEvent::Unpause,
-        // Some("update") => ContainerEvent::Update,
         Some("prune") => ContainerEvent::Prune,
         Some(_) => ContainerEvent::Undefined,
         None => ContainerEvent::Undefined,
