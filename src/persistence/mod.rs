fcdbba88b5509d83a5ba4196defee687d0fea1b9
thomaszub <thomas.zub@outlook.de>
2021-03-28 20:46:38 +0200

    Added impl for repo initial sending and continuous adding/removing of containers

--- src/persistence/mod.rs
+++ src/persistence/mod.rs
@@ -1,15 +1,25 @@
-use tokio::sync::{broadcast, oneshot};
+use tokio::{
+    sync::{
+        broadcast::{
+            self,
+            error::RecvError
+        },
+        oneshot
+    },
+    task
+};
+use tracing::error;
 
 use crate::configuration::Configuration;
-use self:: {
+use crate::docker::{Event, EventType, ContainerEvent};
+use self::{
     no_persistence_repository::NoPersistenceRepository
 };
-use crate::docker::Event;
 
 mod no_persistence_repository;
 mod sled_repository;
 
-pub trait Repository {
+pub trait Repository: Send {
     fn list(&self) -> Vec<String>;
     fn add(&mut self, container_name: String);
     fn delete(&mut self, container_name: String);
@@ -17,12 +27,12 @@ pub trait Repository {
 
 pub async fn spin_up(
     init_sender: oneshot::Sender<Vec<String>>,
-    receiver: broadcast::Receiver<Event>,
+    event_receiver: broadcast::Receiver<Event>,
     conf: &Configuration
 ) {
     let repository = create_repository(conf);
-
-
+    initial(init_sender, repository.as_ref());
+    source(event_receiver, repository).await;
 }
 
 fn create_repository(conf: &Configuration) -> Box<dyn Repository> {
@@ -34,6 +44,41 @@ fn create_repository(conf: &Configuration) -> Box<dyn Repository> {
             Box::new(NoPersistenceRepository {
 
             })
+        }
+    }
+}
+
+fn initial(init_sender: oneshot::Sender<Vec<String>>, repo: &dyn Repository) {
+    if let Err(err) = init_sender.send(repo.list()) {
+        error!("error sending initial vector: {:?}", err);
+    }
+}
+
+async fn source(mut event_receiver: broadcast::Receiver<Event>, mut repo: Box<dyn Repository>) {
+    task::spawn(async move {
+        loop {
+            match event_receiver.recv().await {
+                Ok(event) => {dispatch_event(event, &mut repo)},
+                Err(RecvError::Closed) => break,
+                Err(e) => {
+                    error!("receive failed: {}", e);
+                    continue;
+                }
+            }
+        }
+    });
+}
+
+fn dispatch_event(event: Event, repo: &mut Box<dyn Repository>) {
+    if let EventType::State(container_event) = event.event {
+        match container_event {
+            ContainerEvent::Create
+            | ContainerEvent::Start => repo.add(event.container_name),
+            ContainerEvent::Destroy
+            | ContainerEvent::Die
+            | ContainerEvent::Kill
+            | ContainerEvent::Stop => repo.delete(event.container_name),
+            _ => {}
         }
     }
 }
