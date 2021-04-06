79bbace399081d4e177e485926740cddf55256a1
Alexander Serowy <serowy@hotmail.com>
2021-04-06 08:27:00 +0200

    fixed starting the pipeline

--- src/main.rs
+++ src/main.rs
@@ -18,10 +18,11 @@ async fn main() {
     let (mqtt_sender, mqtt_receiver) = broadcast::channel(100);
     let repo_receiver = mqtt_sender.subscribe();
 
-    persistence::init_task(repo_init_sender, &conf);
-
     docker::task(mqtt_sender, repo_init_receiver).await;
 
     mqtt::task(mqtt_receiver, &conf).await;
     persistence::state_task(repo_receiver, &conf).await;
+
+    // starting the initial process, after all actors are running
+    persistence::init_task(repo_init_sender, &conf);
 }
