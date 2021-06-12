68d2aa4d957b4da95147b6cb1dec8fd39d7635e3
Alexander Serowy <serowy@hotmail.com>
2021-06-12 10:24:16 +0200

    co tests
    Signed-off-by: Alexander Serowy <serowy@hotmail.com>

--- src/docker/events/mod.rs
+++ src/docker/events/mod.rs
@@ -1,10 +1,7 @@
 use std::collections::HashMap;
 
 use bollard::{errors::Error, models::SystemEventsResponse, system::EventsOptions, Docker};
-use tokio::{
-    sync::{broadcast, mpsc},
-    task,
-};
+use tokio::sync::mpsc;
 use tokio_stream::{Stream, StreamExt};
 use tracing::error;
 
@@ -75,121 +72,121 @@ fn get_options() -> EventsOptions<String> {
 // TODO: Fix tests
 /* #[cfg(test)]
 mod must {
-    use std::{
-        sync::{Arc, Mutex},
-        time::Duration,
-    };
+use std::{
+sync::{Arc, Mutex},
+time::Duration,
+};
 
-    use tokio::{sync::broadcast, task};
-    use tokio_stream::StreamExt;
+use tokio::{sync::broadcast, task};
+use tokio_stream::StreamExt;
 
-    use crate::events::{Event, EventType};
+use crate::events::{Event, EventType};
 
-    use super::EventActor;
+use super::EventActor;
 
-    #[test]
-    fn filter_events_for_type_container_only() {
-        // act
-        let options = super::get_options();
-        let mut filters = options.filters.into_iter();
+#[test]
+fn filter_events_for_type_container_only() {
+// act
+let options = super::get_options();
+let mut filters = options.filters.into_iter();
 
-        // assert
-        assert_eq!(
-            Some(("type".to_owned(), vec!["container".to_owned()])),
-            filters.next()
-        );
+// assert
+assert_eq!(
+Some(("type".to_owned(), vec!["container".to_owned()])),
+filters.next()
+);
 
-        assert_eq!(None, filters.next());
-    }
+assert_eq!(None, filters.next());
+}
 
-    #[tokio::test]
-    async fn stop_receive_loop_if_stream_closed() {
-        // arrange
-        let stream = tokio_stream::empty();
-        let (event_sender, _) = broadcast::channel(500);
+#[tokio::test]
+async fn stop_receive_loop_if_stream_closed() {
+// arrange
+let stream = tokio_stream::empty();
+let (event_sender, _) = broadcast::channel(500);
 
-        // act
-        let timeout = tokio::time::timeout(
-            Duration::from_millis(100),
-            super::receive_loop(stream, event_sender),
-        );
+// act
+let timeout = tokio::time::timeout(
+Duration::from_millis(100),
+super::receive_loop(stream, event_sender),
+);
 
-        // assert
-        if timeout.await.is_err() {
-            panic!("future not closed in time");
-        }
-    }
+// assert
+if timeout.await.is_err() {
+panic!("future not closed in time");
+}
+}
 
-    #[tokio::test]
-    async fn pass_messages_through_receive_loop() {
-        // arrange
-        let stream = tokio_stream::iter(vec![
-            vec![Event {
-                container_name: "test1".to_owned(),
-                event: EventType::CpuUsage(1.0),
-            }],
-            vec![Event {
-                container_name: "test2".to_owned(),
-                event: EventType::CpuUsage(2.0),
-            }],
-        ]);
+#[tokio::test]
+async fn pass_messages_through_receive_loop() {
+// arrange
+let stream = tokio_stream::iter(vec![
+vec![Event {
+container_name: "test1".to_owned(),
+event: EventType::CpuUsage(1.0),
+}],
+vec![Event {
+container_name: "test2".to_owned(),
+event: EventType::CpuUsage(2.0),
+}],
+]);
 
-        let (event_sender, mut receiver) = broadcast::channel(500);
+let (event_sender, mut receiver) = broadcast::channel(500);
 
-        // act
-        let timeout = tokio::time::timeout(
-            Duration::from_millis(100),
-            super::receive_loop(stream, event_sender),
-        );
+// act
+let timeout = tokio::time::timeout(
+Duration::from_millis(100),
+super::receive_loop(stream, event_sender),
+);
 
-        task::spawn(async move {
-            if timeout.await.is_err() {
-                panic!("future not closed in time");
-            }
-        });
+task::spawn(async move {
+if timeout.await.is_err() {
+panic!("future not closed in time");
+}
+});
 
-        // assert
-        assert_eq!("test1", receiver.recv().await.unwrap().container_name);
-        assert_eq!("test2", receiver.recv().await.unwrap().container_name);
-    }
+// assert
+assert_eq!("test1", receiver.recv().await.unwrap().container_name);
+assert_eq!("test2", receiver.recv().await.unwrap().container_name);
+}
 
-    #[tokio::test]
-    async fn not_stop_sending_while_getting_errors() {
-        // arrange
-        let counter = Arc::new(Mutex::new(0));
-        let counter_moved = counter.clone();
+#[tokio::test]
+async fn not_stop_sending_while_getting_errors() {
+// arrange
+let counter = Arc::new(Mutex::new(0));
+let counter_moved = counter.clone();
 
-        let stream = tokio_stream::iter(vec![
-            vec![Event {
-                container_name: "test1".to_owned(),
-                event: EventType::CpuUsage(1.0),
-            }],
-            vec![Event {
-                container_name: "test2".to_owned(),
-                event: EventType::CpuUsage(2.0),
-            }],
-        ])
-        .map(|evnts| {
-            let mut data = counter_moved.lock().unwrap();
-            *data += 1;
+let stream = tokio_stream::iter(vec![
+vec![Event {
+container_name: "test1".to_owned(),
+event: EventType::CpuUsage(1.0),
+}],
+vec![Event {
+container_name: "test2".to_owned(),
+event: EventType::CpuUsage(2.0),
+}],
+])
+.map(|evnts| {
+let mut data = counter_moved.lock().unwrap();
+*data += 1;
 
-            evnts
-        });
+evnts
+});
 
-        let (event_sender, receiver) = broadcast::channel(500);
-        drop(receiver); // droping receiver enforces err while sending to channel
+let (event_sender, receiver) = broadcast::channel(500);
+drop(receiver); // droping receiver enforces err while sending to channel
 
-        // act
-        let timeout = tokio::time::timeout(
-            Duration::from_millis(100),
-            super::receive_loop(stream, event_sender),
-        );
+// act
+let timeout = tokio::time::timeout(
+Duration::from_millis(100),
+super::receive_loop(stream, event_sender),
+);
 
-        if timeout.await.is_err() {
-            panic!("future not closed in time");
-        }
+if timeout.await.is_err() {
+panic!("future not closed in time");
+}
 
-        // assert
-        assert_eq!(2, *counter.lock().unwrap());
-    }
+// assert
+assert_eq!(2, *counter.lock().unwrap());
+}
 } */
