d69b2c0153b78ad3f9c13d2016456a6bae153aaa
Alexander Serowy <serowy@hotmail.com>
2021-06-15 11:28:40 +0200

    cfmt
    Signed-off-by: Alexander Serowy <serowy@hotmail.com>

--- src/reaktor/relay.rs
+++ src/reaktor/relay.rs
@@ -5,7 +5,10 @@ use tracing::error;
 pub struct Relay {}
 
 impl Relay {
-    pub async fn new<T: Send + 'static>(mut receiver: mpsc::Receiver<T>, sender: mpsc::Sender<T>) -> Self {
+    pub async fn new<T: Send + 'static>(
+        mut receiver: mpsc::Receiver<T>,
+        sender: mpsc::Sender<T>,
+    ) -> Self {
         tokio::spawn(async move {
             while let Some(message) = receiver.recv().await {
                 if let Err(e) = sender.send(message).await {
